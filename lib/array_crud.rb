fc1fb5bf5c851edf45bddd6d10aee0a5ca100177
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-10 07:22:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  school= ["wow", "arrays!"]
+  school= ["am", "arrays!"]
   school[0]
 end
 
