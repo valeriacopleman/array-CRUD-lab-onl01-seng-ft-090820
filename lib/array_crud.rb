b942f158beb968e5bba9c136526321dac2eed14f
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-10 07:17:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  school = []
   school.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
+  
   school.unshift("wonders")
 end
 
