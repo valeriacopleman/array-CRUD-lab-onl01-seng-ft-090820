b51fb9f74921056c04fa26833ee64d12c9b9ae83
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-10 07:02:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  school_array = ["clever", "epic", "seesaw", "reading eggs"]
+  school_array = ["clever" , "epic" , "seesaw" , "reading"]
 end
 
 def add_element_to_end_of_array(array, element)
-  school_array << "math eggs"
+  school_array << "math"
 end
 
 def add_element_to_start_of_array(array, element)
