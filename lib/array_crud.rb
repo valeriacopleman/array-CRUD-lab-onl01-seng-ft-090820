5fc78d9fa69cd5a0a8df5028971a7c1ed62d7229
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-10 07:16:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  math = ["7", "11"]
-  math.push("arrays!")
+  
+  school.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
