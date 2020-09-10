effed89bf0805deced4787115b94efa8a91587e5
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-10 07:09:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-  school  = ["clever" , "epic" , "seesaw" , "reading"]
+  school = ["clever" , "epic" , "seesaw" , "reading"]
 end
 
 def add_element_to_end_of_array(array, element)
-  school_array << 'math'
+  school << 'math'
 end
 
 def add_element_to_start_of_array(array, element)
-  school_array.unshift("wonders")
+  school.unshift("wonders")
 end
 
 def remove_element_from_end_of_array(array)
